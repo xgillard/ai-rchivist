9162335a0410674502fb9a89a74e7b70740dcf88
xaviergillard <xavier.gillard@uclouvain.be>
2024-05-16 23:45:59 +0200

    work in progress

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # TODO
 
-[ ] Ajouter une personne
-[ ] Supprimer une personne
-[ ] Ajouter une localisation
-[ ] Supprimer une localisation
+[X] Ajouter une personne
+[X] Supprimer une personne
+[X] Ajouter une localisation
+[X] Supprimer une localisation
 [ ] Interactions avec le chatbot
 [ ] Gestion des erreurs lorsque l'output du chatbot est tout pété
 [ ] Effectivement sauver les données labellisées
--- static/machine_in_the_loop.js
+++ static/machine_in_the_loop.js
@@ -1,8 +1,107 @@
+/************************************************************************************************************************************************************************/
+/***** CHATBOX***********************************************************************************************************************************************************/
+/************************************************************************************************************************************************************************/
 function toggle_chatbox() {
     $('#chatbox').toggle()
     $('#chatbtn').toggle()
 }
+/************************************************************************************************************************************************************************/
+/***** PERSON ***********************************************************************************************************************************************************/
+/************************************************************************************************************************************************************************/
+function add_person() {
+    const n       = session_data.persons.length;
+    const example = {
+        "firstname": "John",
+        "lastname" : "Doe",
+        "role"     : "Example",
+        "function" : "Peasant"
+    };
+    session_data.persons.push(example);
+    display_person(example, n);
+}
 
+function display_person(person, n){
+    $("#tab-persons table tbody").append(
+        "<tr>" + 
+            `<td contenteditable="true" data-field="first_name" onblur="session_data.persons[${n}].firstname = $(this).text()">${person.firstname}</td>` +
+            `<td contenteditable="true" data-field="last_name"  onblur="session_data.persons[${n}].lastname  = $(this).text()">${person.lastname}</td>`  +
+            `<td contenteditable="true" data-field="role"       onblur="session_data.persons[${n}].role      = $(this).text()">${person.role}</td>`      +
+            `<td contenteditable="true" data-field="function"   onblur="session_data.persons[${n}].function  = $(this).text()">${person.function}</td>`  +
+            `<td><button type="button"  class="btn btn-danger" onclick="delete_person(${n})"><i class="bi bi-trash3-fill"></i></button></td>`            +
+        "</tr>"
+    )
+}
+function display_all_persons() {
+    const n = session_data.persons.length;
+    $("#tab-persons table tbody").empty()
+
+    for (var i = 0; i < n; i++) {
+        display_person(session_data.persons[i], i);
+    }
+} 
+
+function delete_person(idx) {
+    // actually delete some person
+    var n = session_data.persons.length;
+    if (idx == 0) {
+        session_data.persons.shift();
+    } else {
+        for(var i = idx + 1; i < n; i++) {
+            session_data.persons[i-1] = session_data.persons[i];
+        }
+        session_data.persons.pop()
+    }
+
+    display_all_persons()
+}
+/************************************************************************************************************************************************************************/
+/***** LOCATIONS ********************************************************************************************************************************************************/
+/************************************************************************************************************************************************************************/
+function add_location() {
+    const n       = session_data.locations.length;
+    const example = {
+        "name"    : "Neverland",
+        "loctype" : "Country"
+    };
+    session_data.locations.push(example);
+    display_location(example, n);
+}
+
+function display_location(location, n){
+    $("#tab-locations table tbody").append(
+        "<tr>" + 
+            `<td contenteditable="true" data-field="loc_name" onblur="session_data.locations[${n}].name    = $(this).text()" >${location.name}</td>`   +
+            `<td contenteditable="true" data-field="loctype"  onblur="session_data.locations[${n}].loctype = $(this).text()">${location.loctype}</td>` +
+            `<td><button type="button"  class="btn btn-danger" onclick="delete_location(${n})"><i class="bi bi-trash3-fill"></i></button></td>`        +
+        "</tr>"
+    )
+}
+function display_all_locations() {
+    const n = session_data.locations.length;
+    $("#tab-locations table tbody").empty()
+
+    for (var i = 0; i < n; i++) {
+        display_location(session_data.locations[i], i);
+    }
+} 
+
+function delete_location(idx) {
+    // actually delete some location
+    var n = session_data.locations.length;
+    if (idx == 0) {
+        session_data.locations.shift();
+    } else {
+        for(var i = idx + 1; i < n; i++) {
+            session_data.locations[i-1] = session_data.locations[i];
+        }
+        session_data.locations.pop()
+    }
+
+    display_all_locations()
+}
+/************************************************************************************************************************************************************************/
+/***** NAVIGATION *******************************************************************************************************************************************************/
+/************************************************************************************************************************************************************************/
 function nav_to(tab) {
     var tabs      = ["document", "metadata", "persons", "locations"];
 
@@ -22,7 +121,9 @@ function nav_to(tab) {
         }
     });
 }
-
+/************************************************************************************************************************************************************************/
+/***** UTILS ************************************************************************************************************************************************************/
+/************************************************************************************************************************************************************************/
 function post(url, data) {
     $.ajax({
         url:         url,
