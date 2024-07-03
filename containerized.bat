22d51f01a4f10db8f7ea6c7cd076ab70be3ee88c
xaviergillard <xavier.gillard@uclouvain.be>
2024-07-03 12:07:41 +0200

    fix docker and containerized launcher

--- containerized.bat
+++ containerized.bat
@@ -3,8 +3,8 @@
 :: When deploying in production, please make sure to adapt this script according to your very infrastructure
 :: (which is likely very different from my dev laptop !)
 docker run --name    the-airchivist                                                                                                  ^
-           --mount   "type=bind,src=C:\Users\xavier.gillard\Documents\REPO\ai-rchivist\checkpoints,dst=/opt/airchivist/checkpoints"  ^
-           -e        "CHECKPOINT_DIR=/opt/airchivist/checkpoints/"                                                                   ^
+           --mount   "type=bind,src=C:\Users\xavier.gillard\Documents\REPO\ai-rchivist\data,dst=/opt/airchivist/data"                ^
+           -e        "DATASET_DB=/opt/airchivist/data/dataset.db"                                                                    ^
            -e        "MISTRAL_API_KEY=ABCDEFGHIJKLMNOPQSRTUVWXYZ"                                                                    ^
            -e        "USE_LLM=false"                                                                                                 ^
            -p        8080:8080                                                                                                       ^
